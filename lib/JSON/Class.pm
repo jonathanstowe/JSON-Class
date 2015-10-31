4ae3b423690acb32d00cd4019a35701da2d38d39
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-10-31 23:11:24 +0000

    Fill out pod, add versioning

--- lib/JSON/Class.pm
+++ lib/JSON/Class.pm
@@ -29,20 +29,48 @@ JSON::Class - Role to allow a class to unmarshall/marshall itself from JSON
 
 =head1 DESCRIPTION
 
+This is a simple role that provides methods to instantiate a class from a
+JSON string that (hopefully,) represents it, and to serialise an object of
+the class to a JSON string.  The JSON created from an instance should
+round trip to a new instance with the same values for the "public attributes".
+"Private" attributes (that is ones without accessors,) will be ignored for
+both serialisation and de-serialisation.  The exact behaviour depends on that
+of L<JSON::Marshal|https://github.com/jonathanstowe/JSON-Marshal> and
+L<JSON::Unmarshal|https://github.com/tadzik/JSON-Unmarshal> respectively.
+
 =head1 METHODS
 
+=head2 method from-json
+
+    method from-json(Str $json) returns JSON::Class
+
+Deserialises the provided JSON string, returning a new object, with the
+public attributes initialised with the corresponding values in the JSON
+structure.
+
+If the JSON is not valid or the data cannot be coerced into the correct 
+type for the target class then an exception may be thrown.
+
+=head2 method to-json
+
+    method to-json() returns Str
+
+Serialises the public attributes of the object to a JSON string that
+represents the object, this JSON can be fed to the L<from-json> of the
+class to create a new object with matching (public) attributes.
 
 =end pod
 
-role JSON::Class {
+role JSON::Class:ver<v0.0.1>:auth<github:jonathanstowe> {
+
    use JSON::Unmarshal;
    use JSON::Marshal;
 
-   method from-json(Str $json) {
+   method from-json(Str $json) returns JSON::Class {
       unmarshal($json, self);
    }
 
-   method to-json() {
+   method to-json() returns Str {
        marshal(self);
    }
 }
